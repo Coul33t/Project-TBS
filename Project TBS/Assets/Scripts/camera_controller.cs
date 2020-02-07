e0070c66e97c9ed2873033ab261c0d793e8749d9
Quentin <Coulis1990@gmail.com>
2020-02-07 17:05:12 +0100

    Camera problems yo

---
+++ Project TBS/Assets/Scripts/camera_controller.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camera_controller : MonoBehaviour {
+
+    public Camera cam;
+    private Vector3 destination;
+    private bool must_move = false;
+    private float lerp_coef = 0;
+
+    // Start is called before the first frame update
+    void Start() {
+        Vector3 pos = new Vector3(0, 10, 0);
+        cam.transform.position = pos;
+        cam.transform.LookAt(new Vector3(5, 0, 5));
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (must_move) {
+            lerp_coef += 0.1f;
+
+            GoToDestination(destination, lerp_coef);
+            if (lerp_coef >= 1) {
+                lerp_coef = 0;
+                must_move = false;
+            }
+        }
+
+        // SetCameraToCenter(m_variateurTestX, m_variateurTestY, m_variateurTestZ);
+    }
+
+    public void SetCameraToCenter(float x, float y, float z = 0) {
+        Vector3 center_of_terrain = new Vector3(x, z, y);
+        Vector3 camera_pos = new Vector3(x / 2, 10, y / 2);
+        cam.transform.position = camera_pos;
+        cam.transform.LookAt(center_of_terrain);
+    }
+
+    public void MoveTo(Vector3 destination) {
+        this.destination = destination;
+        must_move = true;
+    }
+
+    public void GoToDestination(Vector3 destination, float lerp_coef) {
+        transform.position = Vector3.Lerp(transform.position, destination, lerp_coef);
+    }
+}
--- Project TBS/Assets/Scripts/map_creation.cs
+++ Project TBS/Assets/Scripts/map_creation.cs
@@ -14,16 +14,32 @@ public class map_creation : MonoBehaviour {
 
     // Start is called before the first frame update
     void Start() {
+
         game_map = new GameObject[size_x, size_y];
-        for (int i = 0; i < 10; i++) {
-            for (int j = 0; j < 10; j++) {
-                GameObject newObj = Instantiate(m_tile) as GameObject;
-                newObj.transform.position = new Vector3(i, 0, j);
-                newObj.transform.SetParent(this.transform);
-                game_map[i, j] = newObj;
+
+        for (int i = 0; i < size_x; i++) {
+            for (int j = 0; j < size_y; j++) {
+                GameObject new_tile = Instantiate(m_tile) as GameObject;
+
+                float elevation = (float)((int)Random.Range(-2.0f, 2.0f));
+                new_tile.transform.position = new Vector3(i, elevation, j);
+                new_tile.transform.SetParent(this.transform);
+
+                Renderer obj_renderer = new_tile.GetComponent<Renderer>();
+                if (Random.Range(0.0f, 1.0f) < 0.5) {
+                    obj_renderer.material.SetColor("_Color", Color.green);
+                }
+
+                else {
+                    obj_renderer.material.SetColor("_Color", Color.grey);
+                }
+
+
+                game_map[i, j] = new_tile;
             }
         }
 
+        Camera.main.GetComponent<camera_controller>().MoveTo(new Vector3(size_x / 2, 0, size_y / 2));
     }
 
     // Update is called once per frame
